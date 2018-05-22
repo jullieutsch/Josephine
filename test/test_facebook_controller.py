ec07a6da9b89e0e90f79761d13d118553a544a1e
Jullie <jullie@localhost.localdomain>
2018-05-21 21:00:04 -0300

    Added tests to controller for comment gathering

--- test/test_facebook_controller.py
+++ test/test_facebook_controller.py
@@ -1,26 +1,30 @@
 import unittest
 from unittest.mock import MagicMock
+from unittest.mock import Mock
 from controllers.facebook_controller import FacebookController
+from modules.facebook_facade import FacebookFacade
 
 class TestFacebookController(unittest.TestCase):
 
 	@classmethod
-	def setUp():
-		self.facebook_facade = FacebookFacade()
+	def setUp(self):
+		self.facebook_facade = Mock()
 
-	def should_call_posts_facade_for_posts_pile_saving(self):
-		facebook_facade.get_all_posts_from_page = MagicMock()
-		facebook_facade.get_page_posts = MagicMock()
+	def test_should_call_posts_facade_for_posts_pile_saving(self):
+		self.facebook_facade.get_all_posts_from_page = MagicMock()
 
-		controller = FacebookController(facebook_facade)
+		controller = FacebookController(self.facebook_facade)
 		controller.save_all_posts_from_a_page_in_a_pile()
 
-		facebook_facade.get_all_posts_from_page.assert_called()
-		facebook_facade.get_page_posts.assert_called()
+		self.facebook_facade.get_all_posts_from_page.assert_called()
 
-	def should_call_comments_facade_for_comment_saving(self):
-		facebook_facade.get_page_posts = MagicMock()
+	def test_should_call_comments_facade_for_comment_saving(self):
+		self.facebook_facade.get_all_comments_from_posts_pile = MagicMock()
+		
+		controller = FacebookController(self.facebook_facade)
+		controller.saves_all_comments_from_unsaved_posts_pile()
 
+		self.facebook_facade.get_all_comments_from_posts_pile.assert_called()
 
 if __name__ == '__main__':
 	unittest.main()
