31ce0ad2ad52633f1f2544b507a8c91e88b4679d
Jullie <jullie@localhost.localdomain>
2018-06-06 19:07:28 -0300

    Added comment increaser for comment by date counter

--- source/modules/comment_analysis.py
+++ source/modules/comment_analysis.py
@@ -2,12 +2,23 @@ import datetime
 
 def get_comments_occurrence(db, some_alias):
 	full_results = db.keys("comments:" + some_alias + "*")
+	comments_per_date = dict()
 
 	for comment_key in full_results:
 		comment = db.hmget(comment_key, "created_at")
-		comment_date = get_comment_date(comment)
+		comment_date = 	get_comment_date(comment)
+
+		increment_date(comment_date, comments_per_date)
+
+	return comments_per_date
 
 def get_comment_date(comment_datetime_str):
 	comment_datetime = datetime.datetime.strptime(
 		comment_datetime_str, '%Y-%m-%dT%H:%M:%S+%f')
-	return comment_datetime.date()
+	return str(comment_datetime.date())
+
+def increment_date(comments_date, comments_per_date):
+	if comments_date not in comments_per_date:
+		comments_per_date[comments_date] = 0	
+	comments_per_date[comments_date] = comments_per_date[comments_date] + 1
+	return comments_per_date
--- test/test_comment_analysis.py
+++ test/test_comment_analysis.py
@@ -9,20 +9,28 @@ class TestCommentAnalysis(unittest.TestCase):
 	def setUp(self):
 		self.comment_analysis = modules.comment_analysis
 		
-	def test_get_comments_occurrence(self):
+	def test_get_comments_occurrence_with_two_records_retrieves_correct_count(self):
 		self.fake_db_definition_with_two_records()
 		self.comment_analysis.get_comment_date = MagicMock()
 
-		self.comment_analysis.get_comments_occurrence(self.db_mock, "some_alias")
+		occurrences = self.comment_analysis.get_comments_occurrence(self.db_mock, "some_alias")
 		
 		self.db_mock.keys.assert_called()
 		self.assertEqual(self.db_mock.hmget.call_count, 2)
 		self.assertEqual(self.comment_analysis.get_comment_date.call_count, 2)
 
 	def test_get_comment_date(self):
-		date = self.comment_analysis.get_comment_date(
-			self.generate_comment_created_datetime())
-		self.assertEqual(str(date), "2017-01-07")
+		comment_date = self.comment_analysis.get_comment_date(
+		self.generate_comment_created_datetime())
+
+		self.assertEqual("2017-01-07", comment_date)
+
+	def test_increment_comment_date(self):
+		ocurrencies = self.comment_analysis.increment_date( "2017-01-07", {})
+		ocurrencies = self.comment_analysis.increment_date("2017-01-07", ocurrencies)
+
+		self.assertEqual(len(ocurrencies), 1)
+		self.assertEqual(ocurrencies["2017-01-07"], 2)
 
 	def fake_db_definition_with_two_records(self):
 		self.db_mock = MagicMock()
@@ -32,16 +40,5 @@ class TestCommentAnalysis(unittest.TestCase):
 
 	def generate_comment_created_datetime(self):
 		return '2017-01-07T23:27:04+0000'
-
-	"""def face_db_ocurrences_generator():
-		return "{[
-			{
-				"message": "this is the first occurrence!"
-			},
-			{
-				"message": "this is the second occurrence!"
-			}	
-		
-		]}"""
 if __name__ == '__main__':
 	unittest.main()
