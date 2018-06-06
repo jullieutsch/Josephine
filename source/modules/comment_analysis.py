ee30c4b6d578e950984c20ebf31d39e7c0e7a5bc
Jullie <jullie@localhost.localdomain>
2018-06-06 18:22:52 -0300

    Added conversor for comment created time

--- source/modules/comment_analysis.py
+++ source/modules/comment_analysis.py
@@ -1,9 +1,13 @@
+import datetime
+
 def get_comments_occurrence(db, some_alias):
 	full_results = db.keys("comments:" + some_alias + "*")
 
 	for comment_key in full_results:
-		comment = db.get(comment_key)
-		#comment_date = get_comment_date(comment)
+		comment = db.hmget(comment_key, "created_at")
+		comment_date = get_comment_date(comment)
 
-def get_comment_date(comment):
-	return comment
+def get_comment_date(comment_datetime_str):
+	comment_datetime = datetime.datetime.strptime(
+		comment_datetime_str, '%Y-%m-%dT%H:%M:%S+%f')
+	return comment_datetime.date()
--- test/test_comment_analysis.py
+++ test/test_comment_analysis.py
@@ -10,18 +10,28 @@ class TestCommentAnalysis(unittest.TestCase):
 		self.comment_analysis = modules.comment_analysis
 		
 	def test_get_comments_occurrence(self):
-		self.fake_db_definition()
+		self.fake_db_definition_with_two_records()
+		self.comment_analysis.get_comment_date = MagicMock()
 
 		self.comment_analysis.get_comments_occurrence(self.db_mock, "some_alias")
-		self.comment_analysis.get_comment_date = MagicMock()
 		
 		self.db_mock.keys.assert_called()
-		self.assertEqual(self.db_mock.get.call_count, 2)
+		self.assertEqual(self.db_mock.hmget.call_count, 2)
+		self.assertEqual(self.comment_analysis.get_comment_date.call_count, 2)
 
-	def fake_db_definition(self):
+	def test_get_comment_date(self):
+		date = self.comment_analysis.get_comment_date(
+			self.generate_comment_created_datetime())
+		self.assertEqual(str(date), "2017-01-07")
+
+	def fake_db_definition_with_two_records(self):
 		self.db_mock = MagicMock()
 		self.db_mock.keys = Mock(return_value = ["first_key", "second_key"])
-		self.db_mock.get = MagicMock()
+		self.db_mock.hmget = MagicMock()
+
+
+	def generate_comment_created_datetime(self):
+		return '2017-01-07T23:27:04+0000'
 
 	"""def face_db_ocurrences_generator():
 		return "{[
